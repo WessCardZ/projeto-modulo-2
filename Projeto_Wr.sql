cfbaf299c46ceec79c00af79709c7b252891f63d
WessCardZ <wesleyofficial@hotmail.com>
2023-06-19 22:14:51 -0300

    Banco de dados
    Coloquei uma API atualizada

--- Projeto_Wr.sql
+++ Projeto_Wr.sql
@@ -29,7 +29,7 @@ CREATE TABLE `localizacao` (
   `cidade` varchar(255) NOT NULL,
   `estado` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,7 @@ CREATE TABLE `localizacao` (
 
 LOCK TABLES `localizacao` WRITE;
 /*!40000 ALTER TABLE `localizacao` DISABLE KEYS */;
-INSERT INTO `localizacao` VALUES (1,'Satuba','AL'),(2,'Rio Largo','AL'),(3,'Rio Largo','AL'),(4,'Rio Largo','AL'),(5,'Rio Largo','AL'),(6,'Rio Largo','AL'),(7,'Rio Largo','AL');
+INSERT INTO `localizacao` VALUES (1,'Satuba','AL'),(2,'Rio Largo','AL'),(3,'Rio Largo','AL'),(4,'Rio Largo','AL'),(5,'Rio Largo','AL'),(6,'Rio Largo','AL'),(7,'Rio Largo','AL'),(8,'Maceió','AL');
 /*!40000 ALTER TABLE `localizacao` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -64,11 +64,39 @@ CREATE TABLE `perfil` (
   UNIQUE KEY `cpf_3` (`cpf`),
   UNIQUE KEY `cpf_4` (`cpf`),
   UNIQUE KEY `cpf_5` (`cpf`),
+  UNIQUE KEY `cpf_6` (`cpf`),
+  UNIQUE KEY `cpf_7` (`cpf`),
+  UNIQUE KEY `cpf_8` (`cpf`),
+  UNIQUE KEY `cpf_9` (`cpf`),
+  UNIQUE KEY `cpf_10` (`cpf`),
+  UNIQUE KEY `cpf_11` (`cpf`),
+  UNIQUE KEY `cpf_12` (`cpf`),
+  UNIQUE KEY `cpf_13` (`cpf`),
+  UNIQUE KEY `cpf_14` (`cpf`),
+  UNIQUE KEY `cpf_15` (`cpf`),
+  UNIQUE KEY `cpf_16` (`cpf`),
+  UNIQUE KEY `cpf_17` (`cpf`),
+  UNIQUE KEY `cpf_18` (`cpf`),
+  UNIQUE KEY `cpf_19` (`cpf`),
   UNIQUE KEY `matricula` (`matricula`),
   UNIQUE KEY `matricula_2` (`matricula`),
   UNIQUE KEY `matricula_3` (`matricula`),
   UNIQUE KEY `matricula_4` (`matricula`),
   UNIQUE KEY `matricula_5` (`matricula`),
+  UNIQUE KEY `matricula_6` (`matricula`),
+  UNIQUE KEY `matricula_7` (`matricula`),
+  UNIQUE KEY `matricula_8` (`matricula`),
+  UNIQUE KEY `matricula_9` (`matricula`),
+  UNIQUE KEY `matricula_10` (`matricula`),
+  UNIQUE KEY `matricula_11` (`matricula`),
+  UNIQUE KEY `matricula_12` (`matricula`),
+  UNIQUE KEY `matricula_13` (`matricula`),
+  UNIQUE KEY `matricula_14` (`matricula`),
+  UNIQUE KEY `matricula_15` (`matricula`),
+  UNIQUE KEY `matricula_16` (`matricula`),
+  UNIQUE KEY `matricula_17` (`matricula`),
+  UNIQUE KEY `matricula_18` (`matricula`),
+  UNIQUE KEY `matricula_19` (`matricula`),
   KEY `localizacao_id` (`localizacao_id`),
   CONSTRAINT `perfil_ibfk_1` FOREIGN KEY (`localizacao_id`) REFERENCES `localizacao` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -80,7 +108,7 @@ CREATE TABLE `perfil` (
 
 LOCK TABLES `perfil` WRITE;
 /*!40000 ALTER TABLE `perfil` DISABLE KEYS */;
-INSERT INTO `perfil` VALUES (1,'Luan Oliveira dos Santos',2020328223,22,'2001-05-17','65444267845','https://drive.google.com/file/d/14-ocurjMHmX9262Cj6Al-p4OTKOoLLIB/view',1),(2,'Alessandra Pereira de França',2022337197,19,'2004-02-02','71396088490','https://drive.google.com/file/d/1SZY5jEWugIiHdnXPtZhuTs2JjwvrkG5J/view',2),(3,'Rebeca Vital de Araújo',20224050,20,'2003-05-17','2022337203','https://drive.google.com/file/d/1mqZprcLJavqBbutab6h7_Dg1QSE8fn5z/view',3),(4,'Robert Alan Morais de Albuquerque',2022336064,18,'2004-08-06','86166593236','https://drive.google.com/file/d/1cnoHuN360kuFe_FBLvY5DAYMnm_08UuZ/view',4),(5,'Wesley da Silva Cardozo',2022336206,19,'2004-04-01','09672739516','https://drive.google.com/file/d/1b7Zmv4SzeslDn-5b8Hqrp6oBDxipNTtg/view',5),(6,'Whydger Silva Ferreira Fernandes',2021333095,25,'1997-09-17','73320026470','https://drive.google.com/file/d/1FcAgFqwmpHuEu8Abmm4pqPRwNqF1QEQp/view',NULL),(7,'Mayara Emillyn Leopoldino da Silva',2022336055,19,'2003-09-27','17731156699','https://drive.google.com/file/d/1tqU8ATXySRmvq0N7ybCKAYgztjd5fZrf/view',6),(8,'Shara Raiane da Silva Santos',2022337188,19,'2004-05-05','51573551490','https://drive.google.com/file/d/1EF4YfKRi3YfAp9eHHamdRA6VgOnObguf/view',7);
+INSERT INTO `perfil` VALUES (1,'Luan Oliveira dos Santos',2020328223,22,'2001-05-17','65444267845','https://web.whatsapp.com/865dc827-5627-4b32-a8e9-47c73728a1a5',1),(2,'Alessandra Pereira de França',2022337197,19,'2004-02-02','71396088490','https://drive.google.com/file/d/1SZY5jEWugIiHdnXPtZhuTs2JjwvrkG5J/view?usp=sharing',2),(3,'Rebeca Vital de Araújo',20224050,20,'2003-05-17','2022337203','https://drive.google.com/file/d/1mqZprcLJavqBbutab6h7_Dg1QSE8fn5z/view',3),(4,'Robert Alan Morais de Albuquerque',2022336064,18,'2004-08-06','86166593236','https://drive.google.com/file/d/1cnoHuN360kuFe_FBLvY5DAYMnm_08UuZ/view',4),(5,'Wesley da Silva Cardozo',2022336206,19,'2004-04-01','09672739516','https://pps.whatsapp.net/v/t61.24694-24/342520251_800788084365254_4643696113060068285_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQD3w9qxymMK3XvgJs2fwIYKzJiNFuJ-OGHiy_dotxZVw&oe=649E1223',5),(6,'Whydger Silva Ferreira Fernandes',2021333095,25,'1997-09-17','73320026470','https://drive.google.com/file/d/1FcAgFqwmpHuEu8Abmm4pqPRwNqF1QEQp/view',8),(7,'Mayara Emillyn Leopoldino da Silva',2022336055,19,'2003-09-27','17731156699','https://drive.google.com/file/d/1tqU8ATXySRmvq0N7ybCKAYgztjd5fZrf/view',6),(8,'Shara Raiane da Silva Santos',2022337188,19,'2004-05-05','51573551490','https://drive.google.com/file/d/1EF4YfKRi3YfAp9eHHamdRA6VgOnObguf/view',7);
 /*!40000 ALTER TABLE `perfil` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -93,4 +121,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-17 19:08:22
+-- Dump completed on 2023-06-19 22:14:12
